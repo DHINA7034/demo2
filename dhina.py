21b3d23faf172dabd45a28133145baebd54b01d3
DHINA7034 <dhinakaran7034@gmail.com>
2025-07-13 14:26:33 +0530

    Update dhina.py

--- dhina.py
+++ dhina.py
@@ -1,5 +1,5 @@
 print("hi dhina")
-a= int(input("enter a number"))
-b= int(input("enter a number"))
-c= a+b
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = a+b
 print("sum of 2 number is :",c)
